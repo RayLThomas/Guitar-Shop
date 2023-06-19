15906bb1d4e8d482ba0d7b7208f5a2db89f4e4fb
Raymond <raythomasclient@gmail.com>
2023-06-19 10:49:26 -0400

    added site footer

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,13 @@
+import './footer.styles.scss'
+
+const Footer = () => {
+
+    return (
+    <footer class="site-footer">
+        <p>Demo Ecommerce site by <a href="https://www.linkedin.com/in/raythomasii/">Raymond Thomas</a> as seen on <a href="https://twitter.com/mdo">GitHub</a>.</p>
+        <p><a href="https://www.raymond-thomas.com/">My Personal Site</a></p>
+    </footer>
+    );
+};
+
+export default Footer;
